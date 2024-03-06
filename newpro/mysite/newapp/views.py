c7ece3895c0379ae6745fcab01cda2411374c505
sel613 <ramasamyselvapriyanka@gmail.com>
2024-03-06 12:10:18 +0530

    dj search and user permissions

--- newpro/mysite/newapp/views.py
+++ newpro/mysite/newapp/views.py
@@ -5,6 +5,11 @@ from django.core.paginator import Paginator
 
 def movie_list(request):
     movie_objects = MovieData.objects.all()
+
+    movie_name = request.GET.get('movie_name')
+    if movie_name != '' and movie_name is not None:
+        movie_objects = movie_objects.filter(name__icontains = movie_name)
+
     paginator = Paginator(movie_objects,4)
     page = request.GET.get('page')
     movie_objects = paginator.get_page(page)
